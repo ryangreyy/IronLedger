1851fa12bcacc3e934e60ba34907343fa2929780
Ryan Grey <ryangrey2004@icloud.com>
2026-08-12 16:59:38 -0400

    Lower mobile home hero further

--- pwa-update.js
+++ pwa-update.js
@@ -2,11 +2,11 @@
 (function () {
   'use strict';
 
-  var VERSION = '337';
+  var VERSION = '338';
   var KEY = 'ig-pwa-version';
-  var EXPECTED_UPDATER = 'pwa-update.js?v=109';
+  var EXPECTED_UPDATER = 'pwa-update.js?v=110';
   var EXPECTED_MANIFEST = 'manifest.json?v=108';
-  var EXPECTED_STYLE = 'styles.css?v=322';
+  var EXPECTED_STYLE = 'styles.css?v=323';
   var EXPECTED_APP = 'app.js?v=230';
   var EXPECTED_SOFT_NAV = 'soft-nav.js?v=6';
   var GATE_STYLE_ID = 'ig-pwa-refresh-gate-style';
