5a0fb88fb0227c1dbecbf46b1461936df2137947
Ryan Grey <ryangrey2004@icloud.com>
2026-08-13 15:12:15 -0400

    Fix initial leaderboard streak render

--- pwa-update.js
+++ pwa-update.js
@@ -2,9 +2,9 @@
 (function () {
   'use strict';
 
-  var VERSION = '345';
+  var VERSION = '346';
   var KEY = 'ig-pwa-version';
-  var EXPECTED_UPDATER = 'pwa-update.js?v=117';
+  var EXPECTED_UPDATER = 'pwa-update.js?v=118';
   var EXPECTED_MANIFEST = 'manifest.json?v=108';
   var EXPECTED_STYLE = 'styles.css?v=328';
   var EXPECTED_APP = 'app.js?v=234';
